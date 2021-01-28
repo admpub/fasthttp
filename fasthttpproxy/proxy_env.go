5f8d2b5a87246ac75ff4eb6088e3d818626f7266
Hank Shen <swhbox@foxmail.com>
2021-01-28 17:45:45 +0800

    update

--- fasthttpproxy/proxy_env.go
+++ fasthttpproxy/proxy_env.go
@@ -11,7 +11,7 @@ import (
 
 	"golang.org/x/net/http/httpproxy"
 
-	"github.com/valyala/fasthttp"
+	"github.com/admpub/fasthttp"
 )
 
 // FasthttpProxyHTTPDialer returns a fasthttp.DialFunc that dials using
